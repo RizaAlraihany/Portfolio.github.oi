2a387f73212dd98c50f1b1d57cbf2e0b029931c5
Riza Alraihany <156495684+RizaAlraihany@users.noreply.github.com>
2024-11-04 13:38:33 +0700

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -4,20 +4,21 @@ function darkMode() {
   const containers = document.querySelectorAll(".container");
   const containerAbout = document.querySelector(".containerAbout");
   const containerContact = document.querySelector(".containerContact");
-  // const asideUl = document.querySelector(".sosmed ul");
   const asideUl = document.querySelector("aside ul");
-  // const sosmed = document.getElementById("aside")
   const iconMode = document.getElementById("iconMode");
   const navLinks = document.querySelectorAll("header.navContainer .navList a"); // Menambahkan baris init a");
   const btnLinks = document.querySelectorAll("header.navContainer a i"); // Menambahkan baris init a");
   const svgIcons = document.querySelectorAll(".svg-icon"); // Mengambil semua elemen dengan kelas svg-icon
+  const footer = document.querySelector(".footer");
+  const mediaQuery = window.matchMedia("(max-width: 768px)"); 
 
   body.classList.toggle("dark-mode");
   header.classList.toggle("navDark");
   containerAbout.classList.toggle("dark-mode");
   asideUl.classList.toggle("navDark");
-  // sosmed.classList.toggle("dark-mode");
   containerContact.classList.toggle("dark-mode");
+  footer.classList.toggle("navDark");
+
   containers.forEach((container) => container.classList.toggle("dark-mode"));
   navLinks.forEach((link) => link.classList.toggle("navDark"));
   btnLinks.forEach((link) => link.classList.toggle("navDark"));
@@ -29,23 +30,31 @@ function darkMode() {
     iconMode.classList.remove("fa-toggle-off");
     iconMode.classList.add("fa-toggle-on");
     iconMode.style.color = "#ECDFCC";
+   
+    if (mediaQuery.matches) {
+      asideUl.style.borderRadius = "8px 8px 0 0";
+    } else {
+      asideUl.style.borderRadius = null;  
+    }
+    
   } else {
     iconMode.classList.remove("fa-toggle-on");
     iconMode.classList.add("fa-toggle-off");
     iconMode.style.color = "#181C14";
+    asideUl.style.borderRadius = null;  
   }
 }
 // toggle active
 const navLinks = document.querySelector(".navList");
+const menu = document.querySelector("#menu");
 // menu diklik
 document.querySelector("#menu").onclick = () => {
   navLinks.classList.toggle("active");
 };
-// menghilangkan nab
-const menu = document.querySelector("#menu");
 
+// menghilangkan nav
 document.addEventListener("click", function (e) {
-  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
+  if (!menu.contains(e.target) && !navLinks.contains(e.target)) {
+    navLinks.classList.remove("active");
   }
 });
